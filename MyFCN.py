3fa702a8b5691f93f49fdb5551e91de35a4d6228
Nhat-Thanh <luonkebenban@gmail.com>
2022-04-12 20:19:45 +0700

    commit code and dataset

--- MyFCN.py
+++ MyFCN.py
@@ -2,35 +2,53 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class MyFCN(nn.Module):
     def __init__(self, n_actions):
         super().__init__()
-        self.conv_1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1)
-        self.conv_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=2, dilation=2)
-        self.conv_3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=3, dilation=3)
-        self.conv_4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=4, dilation=4)
+        self.conv_1 = nn.Conv2d(
+            in_channels=1, out_channels=64, kernel_size=3, padding=1)
+        self.conv_2 = nn.Conv2d(
+            in_channels=64, out_channels=64, kernel_size=3, padding=2, dilation=2)
+        self.conv_3 = nn.Conv2d(
+            in_channels=64, out_channels=64, kernel_size=3, padding=3, dilation=3)
+        self.conv_4 = nn.Conv2d(
+            in_channels=64, out_channels=64, kernel_size=3, padding=4, dilation=4)
 
         # pi network
-        self.conv_5_pi = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=3, dilation=3)
-        self.conv_6_pi = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=2, dilation=2)
-        self.W_xr = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, bias=False)
-        self.W_hr = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, bias=False)
-        self.W_xz = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, bias=False)
-        self.W_hz = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, bias=False)
-        self.W_xh = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, bias=False)
-        self.W_hh = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, bias=False)
-        self.conv_7_pi = nn.Conv2d(in_channels=64, out_channels=n_actions, kernel_size=3, padding=1)
+        self.conv_5_pi = nn.Conv2d(
+            in_channels=64, out_channels=64, kernel_size=3, padding=3, dilation=3)
+        self.conv_6_pi = nn.Conv2d(
+            in_channels=64, out_channels=64, kernel_size=3, padding=2, dilation=2)
+        self.W_xr = nn.Conv2d(in_channels=64, out_channels=64,
+                              kernel_size=3, padding=1, bias=False)
+        self.W_hr = nn.Conv2d(in_channels=64, out_channels=64,
+                              kernel_size=3, padding=1, bias=False)
+        self.W_xz = nn.Conv2d(in_channels=64, out_channels=64,
+                              kernel_size=3, padding=1, bias=False)
+        self.W_hz = nn.Conv2d(in_channels=64, out_channels=64,
+                              kernel_size=3, padding=1, bias=False)
+        self.W_xh = nn.Conv2d(in_channels=64, out_channels=64,
+                              kernel_size=3, padding=1, bias=False)
+        self.W_hh = nn.Conv2d(in_channels=64, out_channels=64,
+                              kernel_size=3, padding=1, bias=False)
+        self.conv_7_pi = nn.Conv2d(
+            in_channels=64, out_channels=n_actions, kernel_size=3, padding=1)
 
         # v network
-        self.conv_5_v = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=3, dilation=3)
-        self.conv_6_v = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=2, dilation=2)
-        self.conv_7_v = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, padding=1)
+        self.conv_5_v = nn.Conv2d(
+            in_channels=64, out_channels=64, kernel_size=3, padding=3, dilation=3)
+        self.conv_6_v = nn.Conv2d(
+            in_channels=64, out_channels=64, kernel_size=3, padding=2, dilation=2)
+        self.conv_7_v = nn.Conv2d(
+            in_channels=64, out_channels=1, kernel_size=3, padding=1)
 
         # reward map convolution
-        self.conv_R = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=33, padding=16, bias=False)
+        self.conv_R = nn.Conv2d(
+            in_channels=1, out_channels=1, kernel_size=33, padding=16, bias=False)
 
     def pi_and_v(self, X_in):
-        X = X_in[:,0:1,:,:]
+        X = X_in[:, 0:1, :, :]
         X = F.relu(self.conv_1(X))
         X = F.relu(self.conv_2(X))
         X = F.relu(self.conv_3(X))
@@ -41,7 +59,7 @@ class MyFCN(nn.Module):
         X_t = F.relu(self.conv_6_pi(X_t))
 
         # ConvGRU
-        H_t1 = X_in[:,-64:,:,:]
+        H_t1 = X_in[:, -64:, :, :]
         R_t = torch.sigmoid(self.W_xr(X_t) + self.W_hr(H_t1))
         Z_t = torch.sigmoid(self.W_xz(X_t) + self.W_hz(H_t1))
         H_tilde_t = torch.tanh(self.W_xh(X_t) + self.W_hh(R_t * H_t1))
--- model.py
+++ model.py
@@ -5,6 +5,11 @@ from State import State
 import numpy as np
 import torch
 
+def MyEntropy(pi):
+    prob = torch.softmax(pi, dim=-1, dtype=torch.float32)
+    log_prob = F.log_softmax(pi, dim=-1)
+    return torch.sum(prob * log_prob, dim=1)
+
 def sample(pi):
     b, n_actions, h, w = pi.shape
     pi_trans = torch.reshape(pi, (-1, n_actions))
@@ -36,11 +41,13 @@ class PixelWiseA3C_InnerState_ConvR:
         self.metric = None
         self.gamma = None
         self.t_max = None
+        self.beta = None
         self.past_reward = {}
         self.past_value = {}
         self.past_log_pi = {}
-    
-    def setup(self, opt_lr, metric, gamma, t_max, model_path, ckpt_path):
+        self.past_entropy = {}
+
+    def setup(self, opt_lr, metric, gamma, t_max, beta, model_path, ckpt_path):
         self.initial_lr = opt_lr
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=opt_lr)
         self.model_path = model_path
@@ -48,13 +55,14 @@ class PixelWiseA3C_InnerState_ConvR:
         self.metric = metric
         self.gamma = gamma
         self.t_max = t_max
+        self.beta = beta
 
     def load_checkpoint(self, ckpt_path):
         if exists(ckpt_path):
             self.ckpt_man = torch.load(ckpt_path)
             self.optimizer.load_state_dict(self.ckpt_man['optimizer'])
             self.model.load_state_dict(self.ckpt_man['model'])
-    
+
     def load_weights(self, filepath):
         if exists(filepath):
             ckpt = torch.load(filepath, map_location=torch.device(self.device))
@@ -76,16 +84,17 @@ class PixelWiseA3C_InnerState_ConvR:
                 pi = F.softmax(pi, 1)
                 actions = torch.argmax(pi, dim=1)
                 current_state.step(actions, inner_state)
-                reward = torch.square(labels - prev_image) * 255 - torch.square(labels - current_state.image) * 255
+                reward = torch.square(
+                    labels - prev_image) * 255 - torch.square(labels - current_state.image) * 255
                 metric = self.metric(labels, current_state.image)
                 rewards.append(reward.numpy())
                 metrics.append(metric.numpy())
-        
+
         reward = np.mean(rewards) * 255
         metric = np.mean(metrics)
-            
+
         return reward, metric
-    
+
     def train(self, train_set, test_set, batch_size, episodes, save_every):
         cur_episode = 0
         if self.ckpt_man is not None:
@@ -96,49 +105,54 @@ class PixelWiseA3C_InnerState_ConvR:
             cur_episode += 1
             noise, labels, _ = train_set.get_batch(batch_size)
             self.current_state.reset(noise)
-            
+
             # noise = self.current_state.tensor
             # noise = noise.to(self.device)
             # labels = labels.to(self.device)
             sum_reward = self.train_step(noise, labels)
-            
-            print(f"{cur_episode} / {max_episode} - sum reward: {sum_reward.numpy() * 255:.6f}")
-            
+
+            print(
+                f"{cur_episode} / {max_episode} - sum reward: {sum_reward.numpy() * 255:.6f}")
+
             if cur_episode % save_every == 0:
                 reward, metric = self.evaluate(test_set, batch_size)
-                print(f"Test - reward: {reward:.6f} - {self.metric.__name__}: {metric:.6f}")
+                print(
+                    f"Test - reward: {reward:.6f} - {self.metric.__name__}: {metric:.6f}")
 
                 torch.save(self.model.state_dict(), self.model_path)
                 torch.save({
-                    'episode' : cur_episode,
-                    'model' : self.model.state_dict(),
-                    'optimizer' : self.optimizer.state_dict()
+                    'episode': cur_episode,
+                    'model': self.model.state_dict(),
+                    'optimizer': self.optimizer.state_dict()
                 }, self.ckpt_path)
-            
-            self.optimizer.param_groups[0]['lr'] = self.initial_lr - ((1 - cur_episode / max_episode) ** 0.9)
+
+            self.optimizer.param_groups[0]['lr'] = self.initial_lr - \
+                ((1 - cur_episode / max_episode) ** 0.9)
 
     def train_step(self, noise, labels):
         # reset gradient
         self.model.train(True)
         self.optimizer.zero_grad()
-        
+
         sum_reward = 0.0
         for t in range(0, self.t_max):
             prev_image = torch.clone(self.current_state.image)
             noise = torch.clone(self.current_state.tensor).to(self.device)
             pi, v, inner_state = self.model.pi_and_v(noise)
-                
+
             actions = sample(pi)
-            
+
             self.current_state.step(actions, inner_state)
             # prev_image = prev_image.to(self.device)
             # cur_image = self.current_state.image.to(self.device)
-            reward = torch.square(labels - prev_image) * 255 - torch.square(labels - self.current_state.image) * 255
+            reward = torch.square(labels - prev_image) * 255 - \
+                torch.square(labels - self.current_state.image) * 255
             self.past_reward[t] = reward.to(self.device)
             self.past_log_pi[t] = MyLogProb(pi, actions)
+            self.past_entropy[t] = MyEntropy(pi)
             self.past_value[t] = v
             sum_reward = sum_reward + torch.mean(reward) * (self.gamma ** t)
-        
+
         pi_loss = 0.0
         v_loss = 0.0
         total_loss = 0.0
@@ -150,11 +164,19 @@ class PixelWiseA3C_InnerState_ConvR:
             R = R + self.past_reward[k]
             Advantage = R - self.past_value[k]
             pi_loss = pi_loss - self.past_log_pi[k] * Advantage
+            pi_loss = pi_loss - 1e-2 * self.past_entropy[k]
             v_loss = v_loss + torch.square(Advantage)
-            total_loss = total_loss - torch.mean(pi_loss + v_loss)
-        
+
+        total_loss = torch.mean(pi_loss + v_loss)
+
         total_loss.backward()
         self.optimizer.step()
         self.model.train(False)
 
+        self.past_log_pi = {}
+        self.past_entropy = {}
+        self.past_value = {}
+        self.past_reward = {}
+
         return sum_reward
+
